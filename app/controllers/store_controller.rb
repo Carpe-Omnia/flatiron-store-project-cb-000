e2a3f12ddb0e55b9ddba198f652c546567bc02d0
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:46:51 +0000

    Automatically backed up by Learn

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,8 @@ class StoreController < ApplicationController
   def index
     @categories = Category.all
     @items = Item.all
+    if user_signed_in?
+      @cart = Cart.find_or_create_by(user_id: current_user.id)
+    end   
   end
 end
