e08a44dce909e3a5abb9efaa950f2e326e47222d
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:18:03 +0000

    Automatically backed up by Learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   def create
-    @item = Item.find_by(id: params[:id])
+    @item = Item.find_by(id: params[:item_id])
     puts(@item)
     puts(current_user)
     flash[:alert] = "Item added to cart"
