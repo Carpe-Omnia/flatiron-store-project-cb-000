691ee7c8ccb7a7624379c5861abbf14f8ee78325
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:00:49 +0000

    Automatically backed up by Learn

--- db/migrate/20140805184430_create_line_items.rb
+++ db/migrate/20140805184430_create_line_items.rb
@@ -1,6 +1,6 @@
 class CreateLineItems < ActiveRecord::Migration[5.2]
   def change
-    create_table line_items do |t|
+    create_table :line_items do |t|
       t.integer :item_id
       t.integer :cart_id
       t.integer :quantity
