fc1cdfd128aa4fdd61e1ccd33d474b75972bbf8e
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 20:17:20 +0000

    Automatically backed up by Learn

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,11 @@
 class CartsController < ApplicationController
   def checkout
+    if current_cart.line_items.length == 0
+      flash[:alert] = "Must have stuff in your cart to checkout"
+      redirect_back(fallback_location: :root)
+    end
+    @order = Order.new(user_id: current_user.id)
+    @order.line_items = current_cart.line_items
+    @order.save
   end
 end
