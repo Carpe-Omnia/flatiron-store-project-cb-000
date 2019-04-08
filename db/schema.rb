63a1cb23b69f1468bcbc442e55b63d5daf8a03dc
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:01:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2019_04_07_224102) do
 
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.integer "inventory"
+    t.integer "price"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "cart_id"
+    t.integer "quantity"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
