4b6e661d02505add27cee884827cd8f255583fbf
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:46:53 +0000

    Automatically backed up by Learn

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,6 @@ class StoreController < ApplicationController
     @items = Item.all
     if user_signed_in?
       @cart = Cart.find_or_create_by(user_id: current_user.id)
-    end   
+    end
   end
 end
