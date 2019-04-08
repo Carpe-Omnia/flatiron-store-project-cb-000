bdb0a131671a1a5c831fe8a4db6ea0d6d3ee946f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:06:13 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2014_08_05_183012) do
+ActiveRecord::Schema.define(version: 2019_04_07_224102) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
   end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.integer "inventory"
     t.integer "price"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "cart_id"
+    t.integer "quantity"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
