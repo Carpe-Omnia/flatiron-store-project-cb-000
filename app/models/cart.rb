93bf7b538b33261432d936068de5c28a4b8b5b82
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:55:22 +0000

    Automatically backed up by Learn

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,12 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
   has_many :items, through: :line_items
+
+  def total
+    total = 0
+    line_items.forEach do |item|
+      total += item.quantity * item.price
+    end
+    return total
+  end
 end
