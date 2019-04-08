51691e48822fade26f0d269c0fa7d978dbbd1d64
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:21:25 +0000

    Automatically backed up by Learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,9 +1,14 @@
 class LineItemsController < ApplicationController
   def create
-    @item = Item.find_by(id: params[:item_id])
-    puts(@item)
-    puts(current_user)
-    flash[:alert] = "Item added to cart"
-    redirect_back(fallback_location: :root)
+    if !user_signed_in
+      flash[:alert] = "Must be signed in to add items to cart"
+      redirect_back(fallback_location: :root)
+    else
+      @item = Item.find_by(id: params[:item_id])
+      puts(@item)
+      puts(current_user)
+      flash[:alert] = "Item added to cart"
+      redirect_back(fallback_location: :root)
+    end
   end
 end
