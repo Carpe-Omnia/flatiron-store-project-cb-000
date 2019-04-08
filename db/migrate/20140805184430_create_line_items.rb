637c714749d083ac3258b2255e3a87d4fdb34b41
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:35:56 +0000

    Automatically backed up by Learn

--- db/migrate/20140805184430_create_line_items.rb
+++ db/migrate/20140805184430_create_line_items.rb
@@ -3,7 +3,7 @@ class CreateLineItems < ActiveRecord::Migration[5.2]
     create_table :line_items do |t|
       t.integer :item_id
       t.integer :cart_id
-      t.integer :quantity, :default => 0
+      t.integer :quantity, :default => 1
     end
   end
 end
