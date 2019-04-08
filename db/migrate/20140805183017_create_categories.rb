0a2a48d7a0a7762301adf2750eeaf9ef01d42809
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:06:03 +0000

    Automatically backed up by Learn

--- db/migrate/20140805183017_create_categories.rb
+++ db/migrate/20140805183017_create_categories.rb
@@ -1,6 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
-    create_table :categories do |t| 
-    end   
+    create_table :categories do |t|
+      t.string :title
+    end
   end
 end
