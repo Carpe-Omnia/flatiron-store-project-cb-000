9c30eec12446170d33ab201f14e40096929a00b1
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:16:35 +0000

    Automatically backed up by Learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,8 @@
 class LineItemsController < ApplicationController
   def create
+    @item = Item.find(params[:id])
+    puts(@item)
+    puts(current_user)
     flash[:alert] = "Item added to cart"
     redirect_back(fallback_location: root_path)
   end
