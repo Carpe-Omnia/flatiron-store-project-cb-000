696064998ea56ff60d7f2b12d5bbb2d8b770395b
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-08 19:36:44 +0000

    Automatically backed up by Learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -7,8 +7,13 @@ class LineItemsController < ApplicationController
     end
     @cart = Cart.find_or_create_by(user_id: current_user.id)
     @item = Item.find_by(id: params[:item_id])
-    @line_item = LineItem.new(item_id: @item.id)
-    @cart.line_items << @line_item
+    @line_item = LineItem.find_or_create_by(item_id: @item.id, cart_id: @cart.id)
+    if @cart.line_items.include?(@line_item)
+      @line_item.quantity += 1
+    else   
+      @cart.line_items << @line_item
+    end    
+    @item.inventory -= 1 
     puts @cart.line_items
     flash[:alert] = "Item added to cart"
     redirect_back(fallback_location: :root)
